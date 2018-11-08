30c27af34e753f77c8b1903147459e0d6d7a00a7
David Haralson <d.m.haralson@ieee.org>
2018-11-08 10:27:39 -0700

    Eval function

--- cogs/owner.py
+++ cogs/owner.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import subprocess
 from util import misc
+import ast
 import cProfile
 import pstats
 import io
@@ -457,6 +458,82 @@ class Owner:
 
         for p in paginator.pages:
             await ctx.send(p)
+
+    def insert_returns(self, body):
+        # insert return stmt if the last expression is a expression statement
+        if isinstance(body[-1], ast.Expr):
+            body[-1] = ast.Return(body[-1].value)
+            ast.fix_missing_locations(body[-1])
+
+        # for if statements, we insert returns into the body and the orelse
+        if isinstance(body[-1], ast.If):
+            insert_returns(body[-1].body)
+            insert_returns(body[-1].orelse)
+
+        # for with blocks, again we insert returns into the body
+        if isinstance(body[-1], ast.With):
+            insert_returns(body[-1].body)
+
+
+    @commands.command()
+    async def eval_fn(self, ctx, *, cmd):
+        """Evaluates input.
+        Input is interpreted as newline seperated statements.
+        If the last statement is an expression, that is the return value.
+        Usable globals:
+          - `bot`: the bot instance
+          - `discord`: the discord module
+          - `commands`: the discord.ext.commands module
+          - `ctx`: the invokation context
+          - `__import__`: the builtin `__import__` function
+        Such that `>eval 1 + 1` gives `2` as the result.
+        The following invokation will cause the bot to send the text '9'
+        to the channel of invokation and return '3' as the result of evaluating
+        >eval ```
+        a = 1 + 2
+        b = a * 2
+        await ctx.send(a + b)
+        a
+        ```
+
+
+        Lifted from https://gist.github.com/nitros12/2c3c265813121492655bc95aa54da6b9"""
+
+        fn_name = "_eval_expr"
+
+        cmd = cmd.strip("` ")
+
+        # add a layer of indentation
+        cmd = "\n".join("\t{}".format(i) for i in cmd.splitlines())
+
+        # wrap in async def body
+        body = "async def {}():\n{}".format(fn_name, cmd)
+
+        parsed = ast.parse(body)
+        body = parsed.body[0].body
+
+        self.insert_returns(body)
+
+        env = {
+            'bot': ctx.bot,
+            'discord': discord,
+            'commands': commands,
+            'ctx': ctx,
+            '__import__': __import__
+        }
+        exec(compile(parsed, filename="<ast>", mode="exec"), env)
+
+        result = (await eval("{}()".format(fn_name), env))
+
+        embed = discord.Embed()
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(format='png'))
+        embed.timestamp = ctx.message.created_at
+        embed.colour = 0xff0000
+
+        embed.add_field(name='Code', value=cmd, inline=False)
+        embed.add_field(name='Result', value=result, inline=False)
+        
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
