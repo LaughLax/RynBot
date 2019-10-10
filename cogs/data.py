fe4fa47318234636b31a4889d2605b25806c685b
David Haralson <d.m.haralson@ieee.org>
2019-10-09 21:00:45 -0600

    Use pools to query db, generate population plot

--- cogs/data.py
+++ cogs/data.py
@@ -58,6 +58,31 @@ class Data(commands.Cog):
     async def data(self, ctx):
         pass
 
+    @staticmethod
+    def _db_fetch_pop_history(db, server_id):
+        with db.get_session() as db:
+            rows = np.array(db.query(Population.datetime, Population.user_count).\
+                            filter(Population.server == server_id).\
+                            order_by(Population.datetime).\
+                            all())
+        return rows
+
+    @staticmethod
+    def make_pop_plot(pop_rows, server_name, file_obj):
+        plt.clf()
+        plt.plot(pop_rows[:, 0], pop_rows[:, 1])
+        plt.xticks(rotation=45)
+        plt.xlabel("Date")
+        plt.ylabel("Member count")
+        plt.title("Membership growth for server: {0}".format(server_name))
+        plt.tight_layout()
+
+        plt.savefig(file_obj, format='png')
+        plt.close()
+
+        file_obj.seek(0)
+        return file_obj
+
     @data.command()
     @commands.bot_has_permissions(attach_files=True)
     async def population(self, ctx, server=None):
@@ -69,27 +94,15 @@ class Data(commands.Cog):
                 await ctx.send('I\'m not in that server.')
                 return
 
-        with self.bot.db.get_session() as db:
-            rows = np.array(db.query(Population.datetime, Population.user_count).\
-                            filter(Population.server == server.id).\
-                            order_by(Population.datetime).\
-                            all())
-
-        await asyncio.sleep(0)
-
-        plt.clf()
-        plt.plot(rows[:, 0], rows[:, 1])
-        plt.xticks(rotation=45)
-        plt.xlabel("Date")
-        plt.ylabel("Member count")
-        plt.title("Membership growth for server: {0.name}".format(server))
-        plt.tight_layout()
+        rows = await self.bot.loop.run_in_executor(None,
+                                                   self._db_fetch_pop_history,
+                                                   self.bot.db, server.id)
 
         with io.BytesIO() as f:
-            plt.savefig(f, format='png')
-            f.seek(0)
+            f = await self.bot.loop.run_in_executor(self.bot.process_pool,
+                                                    self.make_pop_plot,
+                                                    rows, server.name, f)
             await ctx.send(file=discord.File(fp=f, filename="userchart.png"))
-        plt.close()
 
     @data.command()
     @commands.bot_has_permissions(attach_files=True)
@@ -102,13 +115,9 @@ class Data(commands.Cog):
                 await ctx.send('I\'m not in that server.')
                 return
 
-        with self.bot.db.get_session() as db:
-            rows = np.array(db.query(Population.datetime, Population.user_count).\
-                            filter(Population.server == server.id).\
-                            order_by(Population.datetime).\
-                            all())
-
-        await asyncio.sleep(0)
+        rows = await self.bot.loop.run_in_executor(None,
+                                                   self._db_fetch_pop_history,
+                                                   self.bot.db, server.id)
 
         f = lambda x: x.replace(tzinfo=get_localzone())
         rows[:, 0] = np.array(list(map(f, rows[:, 0])))
