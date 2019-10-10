5dd8b089b81e14bf22af852406e7de5ccff4cb0f
David Haralson <d.m.haralson@ieee.org>
2019-10-09 19:32:58 -0600

    Prepare a process pool on startup for multiprocessing

--- Bot_Main.py
+++ Bot_Main.py
@@ -2,12 +2,7 @@ import discord
 from discord.ext import commands
 from util import config
 import logging
-
-logger = logging.getLogger('discord')
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename=config.logfile, encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+from concurrent.futures import ProcessPoolExecutor
 
 game = discord.Activity(type=discord.ActivityType.playing, name=config.activity)
 
@@ -15,14 +10,6 @@ bot = commands.Bot(command_prefix=config.prefix,
                    owner_id=config.owner_id,
                    activity=game,
                    help_command=commands.DefaultHelpCommand(command_attrs={'aliases': ['halp']}))
-
-if __name__ == '__main__':
-    for extension in config.cogs_core:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            print('Failed to load extension {}.'.format(extension))
-            print(e)
 
 
 @bot.event
@@ -39,4 +26,20 @@ async def on_ready():
         await chan.send("Help me <@{}>! I failed to load my logging cog!".format(config.owner_id))
 
 
-bot.run(config.token)
+if __name__ == '__main__':
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(filename=config.logfile, encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+
+    for extension in config.cogs_core:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print('Failed to load extension {}.'.format(extension))
+            print(e)
+
+    bot.process_pool = ProcessPoolExecutor()
+
+    bot.run(config.token)
