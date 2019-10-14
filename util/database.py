e1b05dbff34cd7e3907b67cf52d72b12223fdf11
David Haralson <d.m.haralson@ieee.org>
2019-10-13 21:23:11 -0600

    Create DB structure for task scheduling

--- util/database.py
+++ util/database.py
@@ -73,6 +73,7 @@ class ServerConfig(Base):
 
     stars = relationship('Star', back_populates='server_rel')
     custom_role_chart = relationship('CustomRoleChart', back_populates='server_rel')
+    scheduled_tasks = relationship('ScheduledTasks', back_populates='server_rel')
 
 
 class Star(Base):
@@ -111,6 +112,27 @@ class CustomRoleChart(Base):
                       nullable=False)
 
     server_rel = relationship('ServerConfig', back_populates='custom_role_chart')
+
+
+class ScheduledTasks(Base):
+    __tablename__ = 'scheduled_tasks'
+
+    server = sql.Column(sql.BigInteger,
+                        sql.ForeignKey('server_config.server'),
+                        primary_key=True,
+                        nullable=False)
+    channel = sql.Column(sql.BigInteger,
+                         nullable=False)
+    task_name = sql.Column(sql.String(32),
+                           primary_key=True,
+                           nullable=False)
+    command = sql.Column(sql.String(32),
+                         nullable=False)
+    last_run_msg_id = sql.Column(sql.BigInteger,
+                                 unique=True,
+                                 nullable=False)
+
+    server_rel = relationship('ServerConfig', back_populates='scheduled_tasks')
 
 
 def setup(bot):
