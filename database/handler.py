86cff2e4b2b4f6792a6238ef2f72f5c86cdb251b
David Haralson <d.m.haralson@ieee.org>
2020-04-20 21:45:11 -0600

    Remove silly "provide_db" decorator

--- database/handler.py
+++ database/handler.py
@@ -1,7 +1,6 @@
 from contextlib import contextmanager
 from functools import wraps
 
-import sqlalchemy as sql
 from dogpile.cache import make_region
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
@@ -37,13 +36,6 @@ class DBHandler:
             return await self.execute(None, func, self, *args, **kwargs)
         return wrapper_make_async
 
-    def provide_db(func):
-        @wraps(func)
-        def wrapper_provide_db(self, *args, **kwargs):
-            with self.get_session() as db:
-                return func(self, db, *args, **kwargs)
-        return wrapper_provide_db
-
     @staticmethod
     def get_server_cfg(db, guild_id):
         try:
@@ -58,168 +50,168 @@ class DBHandler:
         return cfg
 
     @async_via_threadpool
-    @provide_db
-    def get_db_size(self,  db):
-        res = db.execute(
-            "SELECT sum((data_length + index_length) / 1024 / 1024) "
-            "AS Size "
-            "FROM information_schema.tables "
-            "WHERE table_schema = 'rynbot' "
-            "GROUP BY table_schema;"
-        ).first().values()[0]
-        return float(res)
+    def get_db_size(self):
+        with self.get_session() as db:
+            res = db.execute(
+                "SELECT sum((data_length + index_length) / 1024 / 1024) "
+                "AS Size "
+                "FROM information_schema.tables "
+                "WHERE table_schema = 'rynbot' "
+                "GROUP BY table_schema;"
+            ).first().values()[0]
+            return float(res)
 
     @async_via_threadpool
-    @provide_db
-    def get_custom_role_list(self, db, guild_id):
-        try:
-            custom_list = db.query(CustomRoleChart.role).\
-                filter(CustomRoleChart.server == guild_id).\
+    def get_custom_role_list(self, guild_id):
+        with self.get_session() as db:
+            try:
+                custom_list = db.query(CustomRoleChart.role).\
+                    filter(CustomRoleChart.server == guild_id).\
+                    all()
+                custom_list = [a[0] for a in custom_list]
+            except NoResultFound:
+                return []
+
+            return custom_list
+
+    @async_via_threadpool
+    def create_task(self, guild_id, channel_id, task_name, command, first_run_msg_id):
+        with self.get_session() as db:
+            self.get_server_cfg(db, guild_id)
+
+            row = ScheduledTasks(server=guild_id,
+                                 channel=channel_id,
+                                 task_name=task_name,
+                                 command=command,
+                                 last_run_msg_id=first_run_msg_id)
+            db.add(row)
+
+    @async_via_threadpool
+    def update_task(self, guild_id, task_name, last_run_msg_id):
+        with self.get_session() as db:
+            try:
+                row = db.query(ScheduledTasks).\
+                    filter(ScheduledTasks.server == guild_id).\
+                    filter(ScheduledTasks.task_name == task_name).\
+                    one_or_none()
+            except MultipleResultsFound as e:
+                raise e
+
+            row.last_run_msg_id = last_run_msg_id
+            db.add(row)
+
+    @async_via_threadpool
+    def delete_task(self, guild_id, task_name):
+        with self.get_session() as db:
+            try:
+                row = db.query(ScheduledTasks).\
+                    filter(ScheduledTasks.server == guild_id).\
+                    filter(ScheduledTasks.task_name == task_name).\
+                    one_or_none()
+            except MultipleResultsFound as e:
+                raise e
+
+            db.delete(row)
+
+    @async_via_threadpool
+    def fetch_task_list(self, guild_id = None):
+        with self.get_session() as db:
+            try:
+                rows = db.query(ScheduledTasks.server,
+                                ScheduledTasks.channel,
+                                ScheduledTasks.task_name,
+                                ScheduledTasks.command,
+                                ScheduledTasks.last_run_msg_id)
+                if guild_id is not None:
+                    rows = rows.filter(ScheduledTasks.server == guild_id)
+                rows = rows.all()
+                # rows = [a[0] for a in rows]
+            except NoResultFound as e:
+                raise e
+
+            return rows
+
+    @async_via_threadpool
+    def fetch_population_history(self, server_id):
+        with self.get_session() as db:
+            rows = db.query(Population.datetime, Population.user_count).\
+                filter(Population.server == server_id).\
+                order_by(Population.datetime).\
                 all()
-            custom_list = [a[0] for a in custom_list]
-        except NoResultFound:
-            return []
 
-        return custom_list
-
-    @async_via_threadpool
-    @provide_db
-    def create_task(self, db, guild_id, channel_id, task_name, command, first_run_msg_id):
-        self.get_server_cfg(db, guild_id)
-
-        row = ScheduledTasks(server=guild_id,
-                             channel=channel_id,
-                             task_name=task_name,
-                             command=command,
-                             last_run_msg_id=first_run_msg_id)
-        db.add(row)
-
-    @async_via_threadpool
-    @provide_db
-    def update_task(self, db, guild_id, task_name, last_run_msg_id):
-        try:
-            row = db.query(ScheduledTasks).\
-                filter(ScheduledTasks.server == guild_id).\
-                filter(ScheduledTasks.task_name == task_name).\
-                one_or_none()
-        except MultipleResultsFound as e:
-            raise e
-
-        row.last_run_msg_id = last_run_msg_id
-        db.add(row)
-
-    @async_via_threadpool
-    @provide_db
-    def delete_task(self, db, guild_id, task_name):
-        try:
-            row = db.query(ScheduledTasks).\
-                filter(ScheduledTasks.server == guild_id).\
-                filter(ScheduledTasks.task_name == task_name).\
-                one_or_none()
-        except MultipleResultsFound as e:
-            raise e
-
-        db.delete(row)
-
-    @async_via_threadpool
-    @provide_db
-    def fetch_task_list(self, db, guild_id = None):
-        try:
-            rows = db.query(ScheduledTasks.server,
-                            ScheduledTasks.channel,
-                            ScheduledTasks.task_name,
-                            ScheduledTasks.command,
-                            ScheduledTasks.last_run_msg_id)
-            if guild_id is not None:
-                rows = rows.filter(ScheduledTasks.server == guild_id)
-            rows = rows.all()
-            # rows = [a[0] for a in rows]
-        except NoResultFound as e:
-            raise e
-
-        return rows
-
-    @async_via_threadpool
-    @provide_db
-    def fetch_population_history(self, db, server_id):
-        rows = db.query(Population.datetime, Population.user_count).\
-            filter(Population.server == server_id).\
-            order_by(Population.datetime).\
-            all()
-
-        return rows
+            return rows
 
     @async_via_threadpool
     @region.cache_on_arguments()
-    @provide_db
-    def fetch_starboard_channel(self, db, server_id):
-        try:
-            starboard = db.query(ServerConfig.starboard).\
-                filter(ServerConfig.server == server_id).\
-                one()[0]
-        except NoResultFound:
-            starboard = None
-        except MultipleResultsFound as e:
-            raise e
+    def fetch_starboard_channel(self, server_id):
+        with self.get_session() as db:
+            try:
+                starboard = db.query(ServerConfig.starboard).\
+                    filter(ServerConfig.server == server_id).\
+                    one()[0]
+            except NoResultFound:
+                starboard = None
+            except MultipleResultsFound as e:
+                raise e
 
-        return starboard
+            return starboard
 
     @async_via_threadpool
     @region.cache_on_arguments()
-    @provide_db
-    def fetch_star_threshold(self, db, server_id):
-        try:
-            star_threshold = db.query(ServerConfig.star_threshold).\
-                filter(ServerConfig.server == server_id).\
-                one()[0]
-        except NoResultFound:
-            star_threshold = 1
-        except MultipleResultsFound as e:
-            raise e
+    def fetch_star_threshold(self, server_id):
+        with self.get_session() as db:
+            try:
+                star_threshold = db.query(ServerConfig.star_threshold).\
+                    filter(ServerConfig.server == server_id).\
+                    one()[0]
+            except NoResultFound:
+                star_threshold = 1
+            except MultipleResultsFound as e:
+                raise e
 
-        return star_threshold
-
-    @async_via_threadpool
-    @provide_db
-    def fetch_star_entry(self, db, server_id, message_id):
-        try:
-            star = db.query(Star.card).\
-                filter(Star.server == server_id).\
-                filter(Star.message == message_id).\
-                one()
-        except NoResultFound:
-            return None
-        except MultipleResultsFound as e:
-            raise e
-
-        return star
+            return star_threshold
 
     @async_via_threadpool
-    @provide_db
-    def delete_star_entry(self, db, server_id, message_id):
-        try:
-            star = db.query(Star).\
-                filter(Star.server == server_id).\
-                filter(Star.message == message_id).\
-                one()
-        except MultipleResultsFound as e:
-            raise e
+    def fetch_star_entry(self, server_id, message_id):
+        with self.get_session() as db:
+            try:
+                star = db.query(Star.card).\
+                    filter(Star.server == server_id).\
+                    filter(Star.message == message_id).\
+                    one()
+            except NoResultFound:
+                return None
+            except MultipleResultsFound as e:
+                raise e
 
-        db.delete(star)
+            return star
 
     @async_via_threadpool
-    @provide_db
-    def create_star_entry(self, db, server_id, channel_id, message_id, author_id, msg_timestamp, card_id):
-        try:
-            star = Star(server=server_id,
-                        channel=channel_id,
-                        message=message_id,
-                        author=author_id,
-                        timestamp=msg_timestamp,
-                        card=card_id)
-            db.add(star)
-        except Exception as e:
-            raise e
+    def delete_star_entry(self, server_id, message_id):
+        with self.get_session() as db:
+            try:
+                star = db.query(Star).\
+                    filter(Star.server == server_id).\
+                    filter(Star.message == message_id).\
+                    one()
+            except MultipleResultsFound as e:
+                raise e
+
+            db.delete(star)
+
+    @async_via_threadpool
+    def create_star_entry(self, server_id, channel_id, message_id, author_id, msg_timestamp, card_id):
+        with self.get_session() as db:
+            try:
+                star = Star(server=server_id,
+                            channel=channel_id,
+                            message=message_id,
+                            author=author_id,
+                            timestamp=msg_timestamp,
+                            card=card_id)
+                db.add(star)
+            except Exception as e:
+                raise e
 
 
 def setup(bot):
